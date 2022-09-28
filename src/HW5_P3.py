54acf9032ef23e01ebcacc111f4d58e727f28ffd
wgeers <wgeers@purdue.edu>
2022-09-28 13:01:56 -0400

    Added Problem 4 files and modified problem 3

--- src/HW5_P3.py
+++ src/HW5_P3.py
@@ -10,19 +10,20 @@ e = 0.100
 def f(E):
     return E - e * np.sin(E) - M
 
-
 def fP(E):
     return 1 - e * np.cos(E)
-
 def fP2(E):
     return e*np.sin(E)
-
 def fP3(E):
     return e*np.cos(E)
-
-
+def D1(E):
+    return -(f(E))/(fP(E))
+def D2(E):
+    return -(f(E))/(fP(E)+0.5*(D1(f)*fP2(E)))
+def D3(E):
+    return -(f(E))/(fP(E)+0.5*(D2(f)*fP2(E))+(1/6)*((D2(f)**2)*fP3(E)))
 while True:
-    Enew = E[-1] - ((f(E)[-1]) / (fP(E)[-1]))
+    Enew = E[-1] + D3(-1)
     E.append(Enew)
     if np.abs(E[-1] - E[-2]) < 0.000001 * (np.pi / 180):
         break

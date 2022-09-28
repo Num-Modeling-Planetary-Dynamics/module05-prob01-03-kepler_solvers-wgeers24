7708416f8f422a27a17fa1f0306f7cc2f395f656
wgeers <wgeers@purdue.edu>
2022-09-28 14:24:20 -0400

    Changed to E

--- src/HW5_P3.py
+++ src/HW5_P3.py
@@ -1,4 +1,4 @@
-#Using Danby's method
+# Using Danby's method
 import numpy as np
 import sympy as sym
 
@@ -10,20 +10,33 @@ e = 0.100
 def f(E):
     return E - e * np.sin(E) - M
 
+
 def fP(E):
     return 1 - e * np.cos(E)
+
+
 def fP2(E):
-    return e*np.sin(E)
+    return e * np.sin(E)
+
+
 def fP3(E):
-    return e*np.cos(E)
+    return e * np.cos(E)
+
+
 def D1(E):
-    return -(f(E))/(fP(E))
+    return -(f(E)) / (fP(E))
+
+
 def D2(E):
-    return -(f(E))/(fP(E)+0.5*(D1(f)*fP2(E)))
+    return -(f(E)) / (fP(E) + 0.5 * (D1(E) * fP2(E)))
+
+
 def D3(E):
-    return -(f(E))/(fP(E)+0.5*(D2(f)*fP2(E))+(1/6)*((D2(f)**2)*fP3(E)))
+    return -(f(E)) / (fP(E) + 0.5 * (D2(E) * fP2(E)) + (1 / 6) * ((D2(E) ** 2) * fP3(E)))
+
+
 while True:
-    Enew = E[-1] + D3(-1)
+    Enew = E[-1] + D3(E[-1])
     E.append(Enew)
     if np.abs(E[-1] - E[-2]) < 0.000001 * (np.pi / 180):
         break
